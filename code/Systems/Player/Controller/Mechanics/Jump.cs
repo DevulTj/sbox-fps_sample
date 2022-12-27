f87a76fd1f2d0c5b5076f033231748e501e68e71
Tony Ferguson <devultj@gmail.com>
2022-12-27 16:45:13 +0000

    Made jump super basic

--- code/Systems/Player/Controller/Mechanics/Jump.cs
+++ code/Systems/Player/Controller/Mechanics/Jump.cs
@@ -8,59 +8,25 @@ namespace Facepunch.Gunfight.Mechanics;
 public partial class JumpMechanic : PlayerControllerMechanic
 {
 	public override int SortOrder => 25;
-
-	private float Cooldown => 0.5f;
 	private float Gravity => 700f;
-
-	private bool Lock = false;
-	
-	public TimeUntil WindupComplete { get; protected set; }
 
 	protected override bool ShouldStart()
 	{
-		if ( Lock ) return true;
-
 		if ( !Input.Pressed( InputButton.Jump ) ) return false;
 		if ( !Controller.GroundEntity.IsValid() ) return false;
-
 		return true;
 	}
 
 	protected override void OnStart()
 	{
-		WindupComplete = 0.1f;
-		Lock = true;
-		Simulate();
-	}
+		float flGroundFactor = 1.0f;
+		float flMul = 250f;
+		float startz = Velocity.z;
 
-	protected override void OnStop()
-	{
-		TimeUntilCanStart = Cooldown;
-	}
+		Velocity = Velocity.WithZ( startz + flMul * flGroundFactor );
+		Velocity -= new Vector3( 0, 0, Gravity * 0.5f ) * Time.Delta;
 
-	protected override void Simulate()
-	{
-		if ( !Controller.GroundEntity.IsValid() )
-		{
-			Lock = false;
-			return;
-		}
-
-		if ( WindupComplete && Controller.GroundEntity.IsValid() )
-		{
-			float flGroundFactor = 1.0f;
-			float flMul = 250f;
-			float startz = Velocity.z;
-
-			Velocity = Velocity.WithZ( startz + flMul * flGroundFactor );
-			Velocity -= new Vector3( 0, 0, Gravity * 0.5f ) * Time.Delta;
-
-			Controller.GetMechanic<WalkMechanic>()
-				.ClearGroundEntity();
-
-			Controller.Player.PlaySound( "sounds/player/foley/gear/player.jump.gear.sound" );
-
-			Lock = false;
-		}
+		Controller.GetMechanic<WalkMechanic>()
+			.ClearGroundEntity();
 	}
 }
