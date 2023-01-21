fe61ae8fd01e9c6c3539fa050fd28b25aad54c2c
sgv <stijn.sv@gmail.com>
2023-01-21 13:56:54 +0100

    fix: remove unstuck as it's gone in the source repo

--- code/Systems/Player/Player.cs
+++ code/Systems/Player/Player.cs
@@ -106,7 +106,6 @@ public partial class Player : AnimatedEntity
 		Components.Create<SprintMechanic>();
 		Components.Create<CrouchMechanic>();
 		Components.Create<InteractionMechanic>();
-		Components.Create<UnstuckMechanic>();
 
 		Components.Create<PlayerAnimator>();
 		var inventory = Components.Create<Inventory>();
